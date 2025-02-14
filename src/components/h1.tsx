6226e73d700621d96b88f30b19c218070ddea68c
omaro6527 <omarharchi4@gmail.com>
2025-02-15 00:04:54 +0100

    on a remplace twmerge et clsx par une fonction cn qui fait les 2

--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -1,11 +1,15 @@
+import { cn } from "@/lib/utils";
+import { twMerge } from "tailwind-merge";
+
 type H1Props = {
+    className: string,
     children: React.ReactNode
     };
 
 
-export default function H1({children}:H1Props) {
+export default function H1({children,className}:H1Props) {
   return (
-    <h1 className="text-3xl lg:text-6xl font-bold tracking-tight"> {children} </h1>
+    <h1 className={cn("text-3xl lg:text-6xl font-bold tracking-tight ",className)}> {children} </h1>
     
   )
 }
